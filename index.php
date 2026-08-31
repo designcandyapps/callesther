b715ef9a171d36c9f8c15e214896793a406f1e0f
Erin Dunn Halligan <erin@designcandy.com>
2026-08-31 09:28:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-
+<?php
+$co=<<<f
+<h5>Call Esther</h5>
+<h3>Personal Assistant</h3>
+<ul>
+  <li>Private driver (Appointments, Airports)</li>
+  <li>Courier (Amazon Returns, Local Delivery*)</li>
+  <li>Haircuts</li>
+  <li>Errands (Groceries)</li>
+</ul>
+<br>
+<i class="i">* Maximum __ lbs. per delivery</i>
+<br><br><br>
+<a href="new.php">Get a ride!</a>
+<!--estherpena53@gmail.com<br>
+832-513-9592-->
+f;
+?>
+<!DOCTYPE html><html>
+<head>
+<title>Call Esther | Personal Assistant</title>
+<meta charset="utf-8"><meta content="ie=edge" http-equiv="x-ua-compatible"><meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
+<script src="inc/jq.js"></script>
+</head>
+<body>
+<?=$co?>
+</body>
+</html>
