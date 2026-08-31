936aa547b19794294b30dfdcaf47097932833cc1
Erin Dunn Halligan <erin@designcandy.com>
2026-08-31 14:05:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
-<?php
-$co=<<<f
-<h5>xCall Esther</h5>
+<script src="https://designcandy.com/_BCK/esther/inc/jq.js"></script>
+<form id="f" method="post">
+<h5>Call Esther</h5>
 <h3>Personal Assistant</h3>
 <ul>
   <li>Private driver (Appointments, Airports)</li>
   <li>Courier (Amazon Returns, Local Delivery*)</li>
   <li>Haircuts</li>
-  <li>Errands (Groceries)</li>
-</ul>
-<br>
-<i class="i">* Maximum __ lbs. per delivery</i>
-<br><br><br>
-<a href="new.php">Get a ride!</a>
-<!--estherpena53@gmail.com<br>
-832-513-9592-->
-f;
-?>
-<!DOCTYPE html><html>
-<head>
-<title>Call Esther | Personal Assistant</title>
-<meta charset="utf-8"><meta content="ie=edge" http-equiv="x-ua-compatible"><meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
-<script src="inc/jq.js"></script>
-</head>
-<body>
-<?=$co?>
-</body>
-</html>
+  <li>Errands (Groceries)</li><br>
+  <i class="i">* Maximum __ lbs. per delivery; Maximum perimeter: ___</i>
+</ul><br>
+<hr style="width:100%; border:1px dotted #bbb"><br><br>
+<div>
+<h3>Sign up now!</h3>
+  <input id="cu" name="cu" placeholder="Name:"><br>
+  <input id="ph" name="ph" placeholder="Phone:"><br>
+  <input id="em" name="em" placeholder="Email:"><br>
+  <button>Submit</button>
+  <span>Go</span>
+</div>
+<div>
+  <input id="pa" name="pa" type=""><br>
+  <input id="pb" name="pb" placeholder="Where are you headed?">
+  <input id="cs" name="cs" type="hidden"><br>
+  <button>Submit</button>
+</div>
+</form>
